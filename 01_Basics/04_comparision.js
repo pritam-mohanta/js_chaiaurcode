5978f0e26df03f50468d589d6205f1b675ba26b9
pritam-mohanta <113913433+pritam-mohanta@users.noreply.github.com>
2023-11-30 20:21:22 +0000

    comparison

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);    //change to number autmatically
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);  // strictly check 
+                         //check data type aswell
